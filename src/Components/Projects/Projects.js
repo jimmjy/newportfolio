4b70d1e2fee1b641f55f8fa5e7862bbcd35c0b8d
jimmjy <jfinkelstein9@me.com>
2019-04-15 20:34:23 -0400

    updated body styling

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -6,40 +6,20 @@ import Footer from '../Footer/Footer';
 import { isMobile } from 'react-device-detect';
 
 class Projects extends Component {
-	state = {
-		mobile: isMobile,
-	};
-
-	// componentDidMount() {
-	// 	this.onWindowChange();
-	// }
-
-	// componentWillUnmount() {
-	// 	this.onWindowChange();
-	// }
-
-	// onWindowChange = () => {
-	// 	window.addEventListener('resize', e => {
-	// 		this.setState({
-	// 			show: e.target.innerWidth > 800,
-	// 		});
-	// 	});
-	// };
-
 	render() {
 		return (
 			<div className="projects-section">
 				<div className="project">
 					<Link className="project__title">Freelance</Link>
-					<div className={`back back-freelance ${this.state.mobile && 'back-opacity'}`} />
+					<div className={`back back-freelance ${isMobile && 'back-opacity'}`} />
 				</div>
 				<div className="project">
 					<Link className="project__title">Hobby Projects</Link>
-					<div className={`back back-hobby-games ${this.state.mobile && 'back-opacity'}`} />
+					<div className={`back back-hobby-games ${isMobile && 'back-opacity'}`} />
 				</div>
 				<div className="project">
 					<Link className="project__title">School Projects</Link>
-					<div className={`back back-school-projects ${this.state.mobile && 'back-opacity'}`} />
+					<div className={`back back-school-projects ${isMobile && 'back-opacity'}`} />
 				</div>
 				<Footer classStyle={'footer-projects-mobile'} />
 			</div>
